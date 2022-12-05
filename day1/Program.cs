447e45730adada781dfb69342146adbd06e7e6e2
Robin Zeijlon <robin.zeijlon@thefarm.se>
2022-12-05 16:39:38 +0100

    day 1

---
+++ day1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input = File.ReadAllText("./day1/data.txt");
+
+var values = input.
+  Split("\n\n").ToList(). //all elfs
+  Select(x => x.Split('\n')). //Each elf
+  Select(
+    x => x.
+    Where(y => int.TryParse(y, out int tmp)).
+    Sum(y => int.Parse(y))). //Sum each elfs calories
+  Order().Reverse().
+  Take(3).Sum();
+
+Console.WriteLine(values);
