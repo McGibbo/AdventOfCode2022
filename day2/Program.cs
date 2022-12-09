ac88c225122bf1a14daeb5b3b3c5247424381563
Robin Zeijlon <robin.zeijlon@thefarm.se>
2022-12-09 14:21:10 +0100

    day2

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = File.ReadAllText("./data.txt");
+
+var moves = 
+input.Replace("\r", "").Split("\n").
+    Select(x => x.Split(" ")).ToList();
+    
+int points = 0;
+
+Dictionary<string, string> win = new() {
+    {"A", "B"},
+    {"B", "C"},
+    {"C", "A"}
+};
+
+Dictionary<string, string> lose = new() {
+    {"A", "C"},
+    {"B", "A"},
+    {"C", "B"}
+};
+
+Dictionary<string, int> pointuse = new(){
+    {"A", 1},
+    {"B", 2},
+    {"C", 3}
+};
+
+Dictionary<string, int> pointwin = new(){
+    {"X", 0},
+    {"Y", 3},
+    {"Z", 6}
+};
+
+foreach (var move in moves)
+{
+    string opponent = move[0];
+    points += pointwin[move[1]];
+    if(move[1] == "X")
+    {
+        points += pointuse[lose[opponent]];
+    }
+    else if (move[1] == "Y")
+    {
+        points += pointuse[opponent];
+    }
+    else if (move[1] == "Z")
+    {
+        points += pointuse[win[opponent]];
+    }
+}
+
+Console.WriteLine(points);
