05aa4b35f7d55d8e54b48f5c267e8f300916fe78
Robin Zeijlon <robin.zeijlon@telia.com>
2022-12-15 11:33:52 +0100

    day3

---
+++ day3/Program.cs
@@ -0,0 +1,26 @@
+﻿string input = File.ReadAllText("./data.txt");
+
+List<string> bags = input.Replace("\r", "").Split("\n").ToList();
+
+int value = bags.
+    Chunk(3).
+    Select(bag => bag[0].Intersect(bag[1]).Intersect(bag[2]).First()).
+    Select(item => getPrio(item)).
+    Sum();
+
+Console.WriteLine(value);
+
+int getPrio(char item)
+{
+    if(char.IsUpper(item))
+        return (int)item - 38;
+    else
+        return (int)item - 96;
+}
+
+char getError(string bag)
+{
+    string compartment1 = bag.Substring(0, bag.Length/2);
+    string compartment2 = bag.Substring(bag.Length/2, bag.Length/2);
+    return compartment1.Intersect(compartment2).First();
+}
