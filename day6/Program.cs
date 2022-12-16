3f1e762a03ae9cb9201fcf979b54b968c5e9800a
Robin Zeijlon <robin.zeijlon@telia.com>
2022-12-16 09:51:09 +0100

    day6

---
+++ day6/Program.cs
@@ -0,0 +1,14 @@
+﻿string input = File.ReadAllText("./data.txt");
+
+HashSet<char> decrypter = new();
+
+for(int i = 0; i <= input.Length; i++)
+{
+    if(!decrypter.Add(input[i]))
+        decrypter.Clear();
+    else if (decrypter.Count() == 14)
+    {
+        Console.WriteLine(i + 1);
+        break;
+    }
+}
